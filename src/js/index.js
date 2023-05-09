4dcc3314e352aacd7c10cac150b3799deeafb2a9
Elena Kuznetsova <kuzn.elena@yahoo.com>
2023-05-09 19:56:04 +0500

    add swiper

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+
+
+// Swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 1.4,
+    loop: true,
+
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.button-right',
+    },
+  
+    mousewheel: true,
+    keyboard: true,
+  });
